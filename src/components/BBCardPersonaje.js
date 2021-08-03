88b21b1efee0bca28005dace164f93d4b104d344
rafaparada <rafa.adalberto@gmail.com>
2021-08-03 11:22:19 -0600

    desectructurando info card

--- src/components/BBCardPersonaje.js
+++ src/components/BBCardPersonaje.js
@@ -1,14 +1,14 @@
 import React from 'react';
-const BBCardPersonaje = ({info}) =>{
+const BBCardPersonaje = ({info:{nombre,birthday,apodo,estado,img}}) =>{
     return(
         <>
             <div className="card" style={{width: '18rem'}}>
-                <img src={info.img} className="card-img-top" alt={info.nombre} />
+                <img src={img} className="card-img-top" alt={nombre} />
                 <div className="card-body">
-                    <h5 className="card-title">{info.nombre}</h5>
-                    <p className="card-text">Fecha Nacimiento: {info.birthday}</p>
-                    <p className="card-text">Apodo: {info.apodo}</p>
-                    <p className="card-text">Estado: {info.estado}</p>
+                    <h5 className="card-title">{nombre}</h5>
+                    <p className="card-text">Fecha Nacimiento: {birthday}</p>
+                    <p className="card-text">Apodo: {apodo}</p>
+                    <p className="card-text">Estado: {estado}</p>
                 </div>
             </div>
         </>
