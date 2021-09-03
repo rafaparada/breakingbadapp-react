281423c0ce831f6246edcfe4de28fcfcf1ce6e78
adalberto_parada <rafa.adalberto@gmail.com>
2021-09-03 16:48:44 -0600

    Retocando codigo

--- src/components/BBCardPersonaje.js
+++ src/components/BBCardPersonaje.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import '../estilos/mystyles.css';
 const BBCardPersonaje = ({info:{nombre,birthday,apodo,estado,img}}) =>{
     return(
         <>
-            <div className="card" style={{width: '18rem'}}>
-                <img src={img} className="card-img-top" alt={nombre} />
-                <div className="card-body">
-                    <h5 className="card-title">{nombre}</h5>
-                    <p className="card-text">Fecha Nacimiento: {birthday}</p>
-                    <p className="card-text">Apodo: {apodo}</p>
-                    <p className="card-text">Estado: {estado}</p>
+            <div className="mycard">
+                <div className="mycard-side front">
+                    <div>
+                        <img src={img} className="myimg" alt={nombre} />
+                    </div>
+                </div>
+                <div className="mycard-side back">
+                    <div>
+                        <h5 className="card-title">{nombre}</h5>
+                        <p className="card-text">Fecha Nacimiento: {birthday}</p>
+                        <p className="card-text">Apodo: {apodo}</p>
+                        <p className="card-text">Estado: {estado}</p>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/BBSelectPersonajes.js
+++ src/components/BBSelectPersonajes.js
@@ -10,19 +10,26 @@ const BBSelectPersonajes = () =>{
     }
     return(
         <>
-            <h2>PERSONAJES DE BREAKING BAD</h2>
-            <br /><br/>
-            { loading ? <Loading /> :
-            <select onChange={setIdPersonaje} className="form-control">
-                {
-                    data.map(p=>(
-                        <option value={p.id} key={p.id}>{p.nombre}</option>
-                    ))
-                }
-            </select>
-            }
-            <hr />
-           {loading1 ? <Loading /> : <BBCardPersonaje info={data1}  />}
+            <div className="container">
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-8 text-center mt-4">
+                        <h2>PERSONAJES DE BREAKING BAD</h2>
+                        { loading ? <Loading /> :
+                            <select onChange={setIdPersonaje} className="form-select">
+                                {
+                                    data.map(p=>(
+                                        <option value={p.id} key={p.id}>{p.nombre}</option>
+                                    ))
+                                }
+                            </select>
+                        }
+                        <hr />
+                        {loading1 ? <Loading /> : <BBCardPersonaje info={data1}  />}
+                    </div>
+                    <div className="col-2"></div>
+                </div>
+            </div>
         </>
     );
 }
